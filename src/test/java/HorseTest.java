8ee1c90a887e26969ddf8112574d6e485560ad96
Sveta Saltykova <libanonair@gmail.com>
2023-08-21 11:22:06 +0300

    delete two lines

--- src/test/java/HorseTest.java
+++ src/test/java/HorseTest.java
@@ -16,34 +16,30 @@ class HorseTest {
     public void getNameTest() {
         String expectedHorseName = "Jane";
         String horseName = horse.getName();
-        assertEquals(expectedHorseName,horseName);
+        assertEquals(expectedHorseName, horseName);
     }
 
     @Test
-    public void getSpeedTest(){
+    public void getSpeedTest() {
         Double expectedSpeed = 10.6;
         Double horseSpeed = horse.getSpeed();
-        assertEquals(expectedSpeed,horseSpeed);
+        assertEquals(expectedSpeed, horseSpeed);
     }
 
     @Test
-    //Проверить, что метод возвращает число, которое было передано третьим параметром в конструктор;
-    //Проверить, что метод возвращает ноль, если объект был создан с помощью конструктора с двумя параметрами;
-    public void getDistanceSecParam(){
-         Horse secHorse = new Horse("Jane",10.6);
-         Double expectedDistance = 0.0;
-         Double horseDistance = secHorse.getDistance();
-         assertEquals(expectedDistance,horseDistance);
+        public void getDistanceSecParam() {
+        Horse secHorse = new Horse("Jane", 10.6);
+        Double expectedDistance = 0.0;
+        Double horseDistance = secHorse.getDistance();
+        assertEquals(expectedDistance, horseDistance);
     }
 
     @Test
-    public void getDistanceThirdParam(){
+    public void getDistanceThirdParam() {
         Double expectedDistance = 200.0;
         Double horseDistance = horse.getDistance();
-        assertEquals(expectedDistance,horseDistance);
-
+        assertEquals(expectedDistance, horseDistance);
     }
-
 
 
 }
