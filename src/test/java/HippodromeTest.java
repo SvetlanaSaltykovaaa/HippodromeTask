c979d5e3314efe3fd5583c28b3852e7888ed3a74
Sveta Saltykova <libanonair@gmail.com>
2023-08-22 10:18:04 +0300

    add isListNull, isListEmpty tests

--- src/test/java/HippodromeTest.java
+++ src/test/java/HippodromeTest.java
@@ -1,22 +1,48 @@
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
+@ExtendWith(MockitoExtension.class)
 class HippodromeTest {
 
 
     List<Horse> horses;
+    Hippodrome hippodrome;
 
     @Test
-    public void listNullException(){
+    public void isListNull() {
+        Throwable thrown = assertThrows(IllegalArgumentException.class,
+                () -> new Hippodrome(null), "Horses cannot be null.");
+        assertEquals("Horses cannot be null.", thrown.getMessage());
+    }
 
-        Horse horse;
+    @Test
 
-//        Throwable thrown = assertThrows(IllegalArgumentException.class,
-//            //    () -> List<Horse> horses = null,
-//                "Speed cannot be negative.");
-//        assertEquals("Speed cannot be negative.", thrown.getMessage());
+    public void isListEmpty() {
+        horses = new ArrayList<>();
+        Throwable thrown = assertThrows(IllegalArgumentException.class,
+                () -> new Hippodrome(horses), "Horses cannot be empty.");
+        assertEquals("Horses cannot be empty.", thrown.getMessage());
+    }
+
+    @Test
+    public void getHorses(){
+
+    }
+
+    @Test
+    public void move(){
+
+    }
+    @Test
+    public void getWinner(){
+
     }
 
 }
