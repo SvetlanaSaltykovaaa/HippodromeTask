a6fb8027d2080f81936c355a73ffd08bf61113df
Sveta Saltykova <libanonair@gmail.com>
2023-08-23 11:08:31 +0300

    add moveInvokeRandom, isDistanceGetValue tests

--- src/test/java/HorseTest.java
+++ src/test/java/HorseTest.java
@@ -3,14 +3,13 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
+import org.mockito.MockedStatic;
 import org.mockito.junit.jupiter.MockitoExtension;
-
 import static org.junit.jupiter.api.Assertions.*;
-
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class HorseTest {
-
     Horse horse;
 
     @BeforeEach
@@ -27,7 +26,7 @@ class HorseTest {
     }
 
     @ParameterizedTest
-    @ValueSource(strings = {"", " "})
+    @ValueSource(strings = {"", " ", "\t\t", "\n\n\n\n"})
     public void isNameBlankSpace(String name) {
         Throwable thrown = assertThrows(IllegalArgumentException.class,
                 () -> new Horse(name, 10, 20),
@@ -72,7 +71,41 @@ class HorseTest {
         assertEquals(200, horse.getDistance());
     }
 
+    @Test
+/*
+    Проверить, что метод вызывает внутри метод getRandomDouble с параметрами 0.2 и 0.9.
+    Для этого нужно использовать MockedStatic и его метод verify;
+*/
+    public void moveInvokeRandom() {
+        try (MockedStatic<Horse> horseMockedStatic = mockStatic(Horse.class)) {
+            horse.move();
+            horseMockedStatic.verify(() -> Horse.getRandomDouble(0.2, 0.9));
+        }
+    }
+
+    @Test
+    /* Проверить, что метод присваивает дистанции значение высчитанное по формуле:
+    distance + speed * getRandomDouble(0.2, 0.9).
+    Для этого нужно замокать getRandomDouble,
+    чтобы он возвращал определенные значения, которые нужно задать параметризовав тест.
+    */
+    public void isDistanceGetValue() {
+        try (MockedStatic<Horse> horseMockedStatic = mockStatic(Horse.class)) {
+            double randomValue = 0.8;
+            horseMockedStatic.when(() -> Horse.getRandomDouble(0.2, 0.9)).thenReturn(randomValue);
+
+            double expectedDistance = (horse.getDistance() + horse.getSpeed() * (Horse.getRandomDouble(0.2, 0.9)));
+            horse.move();
+            assertEquals(expectedDistance, horse.getDistance());
+        }
+    }
+
 
 }
+
+
+
+
+
 
 
