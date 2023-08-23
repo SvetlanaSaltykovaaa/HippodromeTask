ee6edc3ff5b835b5c54b8a299c52d5d62e5adc47
Sveta Saltykova <libanonair@gmail.com>
2023-08-23 11:02:43 +0300

    change mainExecutionTime test

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,19 +1,12 @@
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Timeout;
-
 @Disabled
 class MainTest {
-
     @Test
     @Timeout(value =22)
-    public void mainExecutionTime()  {
-        /* imitation method time execution */
-        try {
-            Thread.sleep(5000);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
+    public void mainExecutionTime() throws Exception {
+        Main.main( new String[] {});
     }
 
 
